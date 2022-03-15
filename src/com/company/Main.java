d9a4dd4064f5c30b2c33cab3d29ac6f21efc0c7e
Mikołaj <266518@student.pwr.edu.pl>
2022-03-16 00:22:55 +0100

    Skończone, tylko zrobić testyu

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,26 +2,43 @@ package com.company;
 
 import com.company.comparators.arrivalComparator;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         processesGenerator generator = new processesGenerator();
         ArrayList<Process> processes;
-        generator.generator(50000,10,25000);
+
+        int n = 50;
+        int maxBurstTime = 100;
+        int maxArrivalTime = 1000;
+        generator.generator(n,maxBurstTime,maxArrivalTime);
         processes = generator.reader();
+        String input = "Input for generator: n = "+ n + " maxBurstTime = " + maxArrivalTime + " maxArrivalTime = " + maxArrivalTime;
+        System.out.println(input);
 
         Collections.sort(processes, new arrivalComparator());
         FCFS fcfs = new FCFS();
-        System.out.println(fcfs.FCFSalgorithms(processes));
+        String fcfsString = "AWT fo FCFS: " + fcfs.FCFSalgorithms(processes);
+        System.out.println(fcfsString);
 
         SJF sjf = new SJF();
-        System.out.println(sjf.SJFalgorithm(processes));
+        String sjfString = "AWT fo SJF: " + sjf.SJFalgorithm(processes);
+        System.out.println(sjfString);
 
         SRJF srjf = new SRJF();
-        System.out.println(srjf.SRJFalgorithm(processes));
+        String srjfString = "AWT fo SRJF: " + srjf.SRJFalgorithm(processes);
+        System.out.println(srjfString);
+
+        RR rr = new RR();
+        String rrString = "AWT fo RR: " + rr.RRalgorithm(processes,2);
+        System.out.println(rrString);
+
+        Zapis zapis = new Zapis();
+        zapis.zapis(input,fcfsString,sjfString,srjfString,rrString);
 
     }
 }
--- src/com/company/RR.java
+++ src/com/company/RR.java
@@ -1,5 +1,48 @@
 package com.company;
 
+import com.company.comparators.burstComparator;
+
+import java.util.ArrayList;
+
 public class RR {
 
+    public double RRalgorithm(ArrayList<Process> lista, int kwant){
+        ArrayList<Process> processes = lista;
+        ArrayList<Process> queue = new ArrayList<>();
+        ArrayList<Process> doneProcesses = new ArrayList<>();
+        Process current = null;
+        int workingTime = 0;
+        int index = 0;
+        double avgWaitingTime = 0;
+
+        while (doneProcesses.size() != processes.size()) {
+            if (index < processes.size()) {
+                while (processes.get(index).getArrivalTime() <= workingTime) {
+                    queue.add(processes.get(index));
+                    index++;
+                    if (index >= processes.size()) {
+                        break;
+                    }
+                }
+            }
+            if (!queue.isEmpty()){
+                current = queue.get(0);
+                queue.remove(0);
+                int tym = workingTime - current.getArrivalTime();
+                avgWaitingTime += tym;
+                current.setWaitingTime(tym + current.getWaitingTime());
+                current.setBurstTime(current.getBurstTime()-kwant);
+                current.setArrivalTime(workingTime+kwant);
+
+                if (current.getBurstTime() <= 0){
+                    doneProcesses.add(current);
+                }else {
+                    queue.add(current);
+                }
+                current = null;
+            }
+            workingTime+= kwant;
+        }
+        return avgWaitingTime/doneProcesses.size();
+    }
 }
